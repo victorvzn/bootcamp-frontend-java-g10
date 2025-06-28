ceb7dabfc1c269052b7bf5bf758bbfff755aaf04
Victor Villazón <victorzn23@gmail.com>
2025-06-27 19:30:51 -0500

    semana03, dia01, javascript intermedio

--- semana02/dia03-01-javascript-intermedio/main.js
+++ semana02/dia03-01-javascript-intermedio/main.js
@@ -1,1 +1,27 @@
-console.log('hola')
+// Tipos de datos no primitivos (Arrays, Objects)
+
+// ARRAYS
+
+// Un arreglo tiene elementos de cualquier tipo de datos: cadenas, números, booleans, null, undefined, arrays, objects, etc.
+
+const arregloVacio = []
+const listaDeNombres = ['Victor', 'Liliana', 'Marcial', 'Diego']
+const listaDeValores = [1, 2, 3, 'Victor', 'Villazón', true, null, undefined, [6, 7, 8]]
+
+console.log(arregloVacio)
+console.log(listaDeNombres)
+console.log(listaDeValores)
+
+// Lectura de los elementos de un arreglo
+
+console.log(listaDeValores[0]) // 1
+console.log(listaDeValores[4]) // Villazón
+console.log(listaDeValores[5]) // true
+console.log(listaDeValores[100]) // undefined
+
+// Escritura en un arreglo
+
+listaDeNombres[2] = 'Paul'
+listaDeNombres[0] = 9999
+
+console.log(listaDeNombres)
