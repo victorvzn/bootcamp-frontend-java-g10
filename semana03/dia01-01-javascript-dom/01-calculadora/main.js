e5a9d4d3aa50af647c2271f9a3e787a6334c8556
Victor Villazón <victorzn23@gmail.com>
2025-06-30 20:16:46 -0500

    semana03, dia01, javascript dom

--- semana03/dia01-01-javascript-dom/01-calculadora/main.js
+++ semana03/dia01-01-javascript-dom/01-calculadora/main.js
@@ -1,9 +1,27 @@
+let numeroActual = '0'
+let operador = ''
+let operando = ''
+
 // Consulta un elemento a la vez
 const inputDisplay = document.querySelector('#inputDisplay')
 const buttons = document.querySelectorAll('.button')
 
+console.log(buttons)
 
+// EVENTOS
+buttons.forEach(function(button) {
 
+  // button.addEventListener(EVENTO, FUNCION)
+  button.addEventListener('click', function(event) {
+    // console.log({ textContent: event.target.textContent })
+
+    const button = event.target
+    const buttonText = button.textContent
+
+    console.log(buttonText)
+  })
+
+})
 
 
 // const inputDisplay = document.getElementById('inputDisplay') // Consulta un elemento por su atributo id
