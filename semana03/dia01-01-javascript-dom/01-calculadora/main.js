b2ae44e2cc4be02ccc8e92599265c1550d7892ab
Victor Villazón <victorzn23@gmail.com>
2025-06-30 20:31:57 -0500

    semana03, dia01, javascript dom

--- semana03/dia01-01-javascript-dom/01-calculadora/main.js
+++ semana03/dia01-01-javascript-dom/01-calculadora/main.js
@@ -19,6 +19,35 @@ buttons.forEach(function(button) {
     const buttonText = button.textContent
 
     console.log(buttonText)
+
+    if ('+-*'.includes(buttonText)) {
+      // 1. Accionamos el operador
+      operador = buttonText
+      operando = Number(numeroActual)
+      numeroActual = '0'
+    // Cuando presionemos el boton '='
+    } else if (buttonText === '=') {
+      // 3. Aquí realizo las operaciones en base al número actual y el operando
+      if (operador === '+') {
+        numeroActual = Number(operando) + Number(numeroActual)
+      } else if (operador === '-') {
+        numeroActual = Number(operando) - Number(numeroActual)
+      } else if (operador === '*') {
+        numeroActual = Number(operando) * Number(numeroActual)
+      }
+    // Cuando presionemos el botn 'AC
+    } else if (buttonText === 'AC') {
+      // Limpiamos operando, operadores y el input
+      numeroActual = '0'
+      operador = ''
+      operando = ''
+    } else {
+      // 2. Se presionó algún número
+      numeroActual = Number(numeroActual + buttonText)
+    }
+
+    inputDisplay.value = numeroActual
+    // console.log({el: this}) // equivalente a event.target
   })
 
 })
