9979212a7af4676e66c27a0504bf303557f66278
Victor Villazón <victorzn23@gmail.com>
2025-07-02 22:03:38 -0500

    semana03, dia02, javascript todo tailwind modules

--- semana03/dia01-02-javascript-dom2/01-todo-app-tailwind-modulos/main.js
+++ semana03/dia01-02-javascript-dom2/01-todo-app-tailwind-modulos/main.js
@@ -2,27 +2,33 @@ const taskInput = document.querySelector('.task__input')
 const taskClear = document.querySelector('.task__clear2')
 const taskList = document.querySelector('.task__list')
 
-let tasks = [
-  {
-    id: 'tarea-1',
-    title: 'Estudiar Javascript',
-    completed: true
-  },
-  {
-    id: 'tarea-2',
-    title: 'Salir al receso a las 9:00pm',
-    completed: false
-  },
-  {
-    id: 'tarea-3',
-    title: 'Realizar un reto para el fin de semana',
-    completed: false
-  }
-]
+let tasks = []
+
+// let tasks = [
+//   {
+//     id: 'tarea-1',
+//     title: 'Estudiar Javascript',
+//     completed: true
+//   },
+//   {
+//     id: 'tarea-2',
+//     title: 'Salir al receso a las 9:00pm',
+//     completed: false
+//   },
+//   {
+//     id: 'tarea-3',
+//     title: 'Realizar un reto para el fin de semana',
+//     completed: false
+//   }
+// ]
 
 function renderTasks() {
   // console.log('Renderizando las tareas...')
   let list = ''
+
+  // TODO: Validar y añadir el contendo "No hay tareas registradas" si no hay tareas en el array tasks.
+
+  
 
   tasks.forEach(function(task, index) {
     // list = list + '<li>' + task.title + '</li>'
@@ -42,11 +48,41 @@ function renderTasks() {
     `
   })
 
-  console.log(list)
+  // console.log(list)
   taskList.innerHTML = list
 }
 
 // TODO: 01 - Al presionar enter en la caja de texto debe agregarse una nueva tarea.
 // Manejar eventos, y renderizar el nuevo elemento.
+
+taskInput.addEventListener('keydown', function(event) {
+  console.log(event.key === 'Enter')
+
+  // const value = taskInput.value
+  const value = event.target.value
+
+  if (value.trim() === '') {
+    taskInput.value = ''
+    return
+  }
+
+  if (event.key === 'Enter') {
+    console.log('Agregando una nueva tarea', value)
+
+    const newTask = {
+      id: crypto.randomUUID(),
+      title: value,
+      completed: false
+    }
+
+    tasks.push(newTask)
+
+    console.log(tasks)
+
+    taskInput.value = ''
+
+    renderTasks()
+  }
+})
 
 renderTasks()
