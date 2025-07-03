a0dfae30bafff4cbfe18b92ed5ece5826bc6b076
Victor Villazón <victorzn23@gmail.com>
2025-07-02 20:50:33 -0500

    semana03, dia02, javascript todo tailwind modules

--- semana03/dia01-02-javascript-dom2/01-todo-app-tailwind-modulos/main.js
+++ semana03/dia01-02-javascript-dom2/01-todo-app-tailwind-modulos/main.js
@@ -1,1 +1,35 @@
-console.log('hola js')
+const taskInput = document.querySelector('.task__input')
+const taskClear = document.querySelector('.task__clear2')
+const taskList = document.querySelector('.task__list')
+
+let tasks = [
+  {
+    id: 'tarea-1',
+    title: 'Estudiar Javascript',
+    completed: true
+  },
+  {
+    id: 'tarea-2',
+    title: 'Salir al receso a las 9:00pm',
+    completed: false
+  },
+  {
+    id: 'tarea-3',
+    title: 'Realizar un reto para el fin de semana',
+    completed: false
+  }
+]
+
+function renderTasks() {
+  // console.log('Renderizando las tareas...')
+  let list = ''
+
+  tasks.forEach(function(task, index) {
+    list = list + '<li>Tarea</li>'
+  })
+
+  console.log(list)
+  taskList.innerHTML = list
+}
+
+renderTasks()
