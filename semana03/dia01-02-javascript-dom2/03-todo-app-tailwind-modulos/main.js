dc34b924a83ba0f97179492af0472f2dcb3ac046
Victor Villazón <victorzn23@gmail.com>
2025-07-04 21:00:32 -0500

    semana03, dia03, javascript imc app

--- semana03/dia01-02-javascript-dom2/03-todo-app-tailwind-modulos/main.js
+++ semana03/dia01-02-javascript-dom2/03-todo-app-tailwind-modulos/main.js
@@ -4,6 +4,8 @@ const taskInput = document.querySelector('.task__input')
 const taskClear = document.querySelector('.task__clear')
 const taskList = document.querySelector('.task__list')
 
+// Localstorage
+
 let tasks = []
 
 // let tasks = [
