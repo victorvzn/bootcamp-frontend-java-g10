420a9d27b952073a88409a34ddf3264689ac5b0f
Victor Villazón <victorzn23@gmail.com>
2025-07-07 21:52:09 -0500

    semana04, dia01, javascript fetch pokemons app

--- semana04/dia01-02-js-fetch-pokemons-app/main.js
+++ semana04/dia01-02-js-fetch-pokemons-app/main.js
@@ -3,16 +3,54 @@ const fetchPokemons = async () => {
 
   const response = await fetch(url) // Devuelve una promesa
 
-  return await response.json()
+  const data = await response.json()
+
+  const dataResults = data.results.map(pokemon => {
+    const id = pokemon.url.split('/').at(6)
+    const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+
+    return {
+      ...pokemon,
+      id,
+      image
+    }
+  })
+
+  console.log({ dataResults })
+
+  return {
+    ...data,
+    results: dataResults
+  }
 }
 
 const renderPokemons = (pokemons = []) => {
-
   // TODO: 01 - Renderizar cada pokemon en #pokemonList
+  const pokemonList = document.querySelector('#pokemonList')
 
+  let elements = ''
+
+  pokemons.forEach(pokemon => {
+    elements += `
+      <article class="pokemon-item">
+        <h2>#${pokemon.id} - ${pokemon.name}</h2>
+        <img
+          src="${pokemon.image}"
+          width="80"
+          height="80"
+        />
+        <div class="pokemon-item__buttons">
+          <button>Favorite</button>
+          <button>Ver</button>
+        </div>
+      </article>
+    `
+  })
+
+  pokemonList.innerHTML = elements
 }
 
 fetchPokemons()
   .then(data => {
-    renderPokemons(data)
+    renderPokemons(data.results)
   })
