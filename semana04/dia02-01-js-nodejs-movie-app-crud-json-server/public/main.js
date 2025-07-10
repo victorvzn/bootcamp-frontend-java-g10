ed55bf8e8ec9444d76dc4d2c3da734bdf637ca63
Victor Villazón <victorzn23@gmail.com>
2025-07-09 22:42:25 -0500

    semana04, dia01, javascript node movie app crud json server

--- semana04/dia02-01-js-nodejs-movie-app-crud-json-server/public/main.js
+++ semana04/dia02-01-js-nodejs-movie-app-crud-json-server/public/main.js
@@ -6,13 +6,52 @@ const fetchPeliculas = async () => {
   return await reponse.json() // Tranformamos el json a un objeto JS
 }
 
+const renderPeliculas = (peliculas = []) => {
+  const elPeliculasList = document.querySelector('.peliculas__list')
+
+  let elementos = ''
+
+  peliculas.forEach(pelicula => {
+    elementos += `
+      <tr>
+        <td>${pelicula.id}</td>
+        <td>
+          <img
+            src="${pelicula.imagen}"
+            width="100"
+            height="150"
+            onerror="this.src='https://placehold.co/100x150'"
+          />
+        </td>
+        <td>
+          <strong>${pelicula.nombre}</strong>
+          <div class="text-small">
+            <strong>Estreno:</strong> ${pelicula.estreno}
+          </div>
+          <div class="text-small">
+            <strong>Resumen:</strong> ${pelicula.resumen}
+          </div>
+        </td>
+        <td>
+          <div class="flex gap-0.5">
+            <button class="peliculas__edit">✏</button>
+            <button class="peliculas__remove">❌</button>
+          </div>
+        </td>
+      </tr>
+    `
+  })
+
+  elPeliculasList.innerHTML = elementos
+}
+
 // Ejemplo usando promesas
 
 fetchPeliculas()
   .then(peliculas => {
     // TODO: Creen la función renderPeliculas para mostrar los datos de las peliculas en la tabla.
 
-    console.log(peliculas)
+    renderPeliculas(peliculas)
   })
 
 
