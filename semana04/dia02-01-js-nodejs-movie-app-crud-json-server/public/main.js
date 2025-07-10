9acd8a4c2dd11e524bef0849710dbab7d80404a3
Victor Villazón <victorzn23@gmail.com>
2025-07-09 22:10:07 -0500

    semana04, dia01, javascript node movie app crud json server

--- semana04/dia02-01-js-nodejs-movie-app-crud-json-server/public/main.js
+++ semana04/dia02-01-js-nodejs-movie-app-crud-json-server/public/main.js
@@ -1,1 +1,19 @@
-console.log('JS')
+const fetchPeliculas = async () => {
+  const url = 'http://localhost:3000/peliculas'
+
+  const reponse = await fetch(url) // Por defecto está usando el método GET
+
+  return await reponse.json() // Tranformamos el json a un objeto JS
+}
+
+// Ejemplo usando promesas
+
+fetchPeliculas()
+  .then(peliculas => {
+    // TODO: Creen la función renderPeliculas para mostrar los datos de las peliculas en la tabla.
+
+    console.log(peliculas)
+  })
+
+
+// Ejemplo usando async/await
