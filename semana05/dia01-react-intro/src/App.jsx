24d83f44657c13149707354d53a2e8c11838a9fa
Victor Villazón <victorzn23@gmail.com>
2025-07-14 20:08:55 -0500

    semana05, dia01, react intro

--- semana05/dia01-react-intro/src/App.jsx
+++ semana05/dia01-react-intro/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// JSX (JAVASCRIPT XML)
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -16,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React (hola)</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
