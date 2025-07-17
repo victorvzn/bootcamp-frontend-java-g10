17741e7ce7b9b2c7edd03c9fffbd79f0f24b1d3f
Victor Villazón <victorzn23@gmail.com>
2025-07-16 20:32:16 -0500

    semana05, dia02, react todo app

--- semana05/dia02-01-react-todo-app/src/App.jsx
+++ semana05/dia02-01-react-todo-app/src/App.jsx
@@ -33,22 +33,28 @@ export default function App() {
       <TodoHeader title="Todo APP + React + Tailwind" />
   
       <ul className="flex flex-col gap-2 mt-4">
-        <li className="flex gap-2 bg-yellow-200 p-2 rounded-lg">
-          <input
-            type="checkbox"
-          />
-          <div className="w-full flex justify-between items-center">
-            <span className="font-medium">
-              Tarea 1
-            </span>
-            <button
-              className="bg-red-300 rounded-lg px-2 py-1 cursor-pointer"
-            >
-              ❌
-            </button>
-          </div>
-        </li>
+        {todos.map(todo => {
+          return (
+            <li key={todo.id} className="flex gap-2 bg-yellow-200 p-2 rounded-lg">
+              <input
+                type="checkbox"
+              />
+              <div className="w-full flex justify-between items-center">
+                <span className="font-medium">
+                  {todo.title}
+                </span>
+                <button
+                  className="bg-red-300 rounded-lg px-2 py-1 cursor-pointer"
+                >
+                  ❌
+                </button>
+              </div>
+            </li>
+          )
+        })}
       </ul>
+
+      <pre className="mt-4">{JSON.stringify(todos, null, 2)}</pre>
     </main>
   )
 }
