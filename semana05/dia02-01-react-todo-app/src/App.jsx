2a1c071cd6bfac02c49afb86530ced5d4dea3137
Victor Villazón <victorzn23@gmail.com>
2025-07-16 20:18:11 -0500

    semana05, dia02, react todo app

--- semana05/dia02-01-react-todo-app/src/App.jsx
+++ semana05/dia02-01-react-todo-app/src/App.jsx
@@ -24,14 +24,31 @@ export default function App() {
 
   const [todos, setTodos] = useState(DEFAULT_TODOS)
 
-  console.log(todos)
+  // console.log(todos)
 
   return (
     <main 
       className="bg-yellow-100 w-[400px] mx-auto mt-10 border border-yellow-400 rounded-lg shadow-md p-4"
     >
       <TodoHeader title="Todo APP + React + Tailwind" />
-      Todo App
+  
+      <ul className="flex flex-col gap-2 mt-4">
+        <li className="flex gap-2 bg-yellow-200 p-2 rounded-lg">
+          <input
+            type="checkbox"
+          />
+          <div className="w-full flex justify-between items-center">
+            <span className="font-medium">
+              Tarea 1
+            </span>
+            <button
+              className="bg-red-300 rounded-lg px-2 py-1 cursor-pointer"
+            >
+              ❌
+            </button>
+          </div>
+        </li>
+      </ul>
     </main>
   )
 }
