c01c1b5f26fda3ea4a3db6b1fa63216328f6129b
Victor Villazón <victorzn23@gmail.com>
2025-07-16 20:00:00 -0500

    semana05, dia02, react todo app

--- semana05/dia02-01-react-todo-app/src/App.jsx
+++ semana05/dia02-01-react-todo-app/src/App.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react"
+import { TodoHeader } from "./components/TodoHeader"
+
 export default function App() {
   // Logica de la función
 
+  const DEFAULT_TODOS = [
+    {
+      id: '1',
+      title: 'Aprender Javascript',
+      completed: true
+    },
+    {
+      id: '2',
+      title: 'Aprender CSS',
+      completed: false
+    },
+    {
+      id: '3',
+      title: 'Aprender React.js + Tailwind',
+      completed: true
+    }
+  ]
+
+  const [todos, setTodos] = useState(DEFAULT_TODOS)
+
+  console.log(todos)
+
   return (
-    <div className="text-center text-red-500 bg-amber-300 text-3xl">Todo App</div>
+    <main 
+      className="bg-yellow-100 w-[400px] mx-auto mt-10 border border-yellow-400 rounded-lg shadow-md p-4"
+    >
+      <TodoHeader />
+      Todo App
+    </main>
   )
 }
---
+++ semana05/dia02-01-react-todo-app/src/components/TodoHeader.jsx
@@ -0,0 +1,9 @@
+export const TodoHeader = () => {
+  return (
+    <header>
+      <h1 className="text-xl font-bold underline">
+        TodoHeader
+      </h1>
+    </header>
+  )
+}
