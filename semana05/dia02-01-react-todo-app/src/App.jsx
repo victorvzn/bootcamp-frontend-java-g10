4d7fd49bdc3cceac78621693d89bf3bcff8af0fb
Victor Villazón <victorzn23@gmail.com>
2025-07-16 19:36:42 -0500

    semana05, dia02, react todo app

--- semana05/dia02-01-react-todo-app/src/App.jsx
+++ semana05/dia02-01-react-todo-app/src/App.jsx
@@ -2,6 +2,6 @@ export default function App() {
   // Logica de la función
 
   return (
-    <div>App</div>
+    <div className="text-center text-red-500 bg-amber-300 text-3xl">Test App</div>
   )
 }
