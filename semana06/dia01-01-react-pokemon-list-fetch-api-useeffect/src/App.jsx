9e0237d70ac6fd2317b78cc1caa21bcf1ccfe653
Victor Villazón <victorzn23@gmail.com>
2025-07-21 19:56:27 -0500

    semana06, dia01, react pokemon list fetch useeffect

--- semana06/dia01-01-react-pokemon-list-fetch-api-useeffect/src/App.jsx
+++ semana06/dia01-01-react-pokemon-list-fetch-api-useeffect/src/App.jsx
@@ -1,5 +1,46 @@
+import { useEffect, useState } from "react"
+
 export default function App() {
+  const [counter, setCounter] = useState(0)
+  const [pokemons, setPokemons] = useState([])
+  // const [counter2, setCounter2] = useState(0)
+
+  // Manejar el ciclo de vida de nuestro componente: Creación, actualiza, destruye
+  // useEffect(FN, DEPENDECIES)
+
+  // useEffect(() => {
+    // console.log('Se ejecute la primera vez')
+  // }, [counter2])
+
+  const fetchPokemons = async () => {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon')
+    
+    return await response.json()
+  }
+
+  useEffect(() => {
+    fetchPokemons()
+      .then((data) => setPokemons(data.results))
+  }, [])
+
   return (
-    <div>App</div>
+    <>
+      <div className="flex flex-col gap-3">
+        {/* <button className="bg-blue-500 p-4" onClick={() => setCounter(counter + 1)}>{counter}</button> */}
+        {/* <button className="bg-red-500 p-4" onClick={() => setCounter2(counter2 + 1)}>{counter2}</button> */}
+
+        <h1>Pokemon List</h1>
+
+        <ul>
+          {pokemons.map(pokemon => {
+            return (
+              <li key={pokemon.url}>{pokemon.name}</li>
+            )
+          })}
+        </ul>
+
+      </div>
+      <pre>{JSON.stringify(pokemons, null, 2)}</pre>
+    </>
   )
 }
