3058c41d8f23c5f948a115a03b8d9527c0cc372a
Victor Villazón <victorzn23@gmail.com>
2025-07-23 20:22:14 -0500

    semana06, dia02, react custom hooks con ejemplos

--- semana06/dia02-01-react-custom-hooks-ejemplos/src/components/ProductList.jsx
+++ semana06/dia02-01-react-custom-hooks-ejemplos/src/components/ProductList.jsx
@@ -12,14 +12,28 @@ export default function ProductList() {
     console.log('Se ejecuta al crearse el componente')
     fetch('https://dummyjson.com/products')
       .then(response => response.json())
-      .then(data => setProducts(data))
+      .then(data => setProducts(data.products))
   }, [])
 
   return (
-    <section>
-      <h1>Product List</h1>
+    <section className="border p-3 mt-4">
+      <h1 className="text-xl font-medium text-center">Product List</h1>
+
+      <ul>
+        {products.map(product => {
+          return (
+            <li key={product.id}>
+              <h5 className="font-medium">{product.id} - {product.title}</h5>
+              <p>{product.description}</p>
+              <img src={product.thumbnail} width={100} />
+            </li>
+          )
+        })}
+      </ul>
 
       {/* TODO: Listar los productos del endpoint products (https://dummyjson.com/products) */}
+
+      <hr className="my-4" />
 
       <pre>{JSON.stringify(products, null, 2)}</pre>
     </section>
