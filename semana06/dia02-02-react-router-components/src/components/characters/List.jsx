fe8a59ee4892adde100be6d70751a74ee0963fa9
Victor Villazón <victorzn23@gmail.com>
2025-07-23 22:00:17 -0500

    semana06, dia02, react router components

--- semana06/dia02-02-react-router-components/src/components/characters/List.jsx
+++ semana06/dia02-02-react-router-components/src/components/characters/List.jsx
@@ -1,7 +1,28 @@
+import { useEffect } from "react"
+import { fetchCharacters } from "../../services/characters"
+import { useState } from "react"
+
 export default function List() {
   // TODO: Renderizar los items del resultado de personajes en un grilla de 3x3
+  const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    fetchCharacters()
+      // .then(data => setCharacters(data.items))
+      .then(({ items }) => setCharacters(items))
+  }, [])
 
   return (
-    <div>List</div>
+    <section>
+      {characters.map(character => {
+        return (
+          <article>
+            <img src={character.image} width={50} />
+            <h2>#{character.id} - {character.name}</h2>
+            <p>{character.race}</p>
+          </article>
+        )
+      })}
+    </section>
   )
 }
