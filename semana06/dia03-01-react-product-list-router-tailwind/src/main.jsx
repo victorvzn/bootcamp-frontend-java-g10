52fd23e0873d4f66a5c7bb73d83b399abe8ba946
Victor Villazón <victorzn23@gmail.com>
2025-07-25 19:31:17 -0500

    semana06, dia03, react product list router components tailwind

--- semana06/dia03-01-react-product-list-router-tailwind/src/main.jsx
+++ semana06/dia03-01-react-product-list-router-tailwind/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import { BrowserRouter, Routes, Route } from 'react-router'
+import LoginPage from './pages/LoginPage.jsx'
+import HomePage from './pages/HomePage.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<LoginPage />} />
+
+      <Route path='/home' element={<HomePage />} />
+    </Routes>
+  </BrowserRouter>,
 )
