a1859804ff44645f0c377c07ef24da713aeafd8b
Victor Villazón <victorzn23@gmail.com>
2025-07-25 19:40:21 -0500

    semana06, dia03, react product list router components tailwind

--- semana06/dia03-01-react-product-list-router-tailwind/src/pages/LoginPage.jsx
+++ semana06/dia03-01-react-product-list-router-tailwind/src/pages/LoginPage.jsx
@@ -1,5 +1,37 @@
 export default function LoginPage() {
   return (
-    <div>LoginPage</div>
+    <main>
+      <div>
+        <h2>Product List App</h2>
+
+        <p>Ingresa un nombre de usuario y password</p>
+
+        <form>
+          <label>
+            Username
+            <input
+              type="text"
+              name="username"
+              placeholder="jhondoe"
+            />
+          </label>
+
+          <label>
+            Password
+            <input
+              type="text"
+              name="password"
+              placeholder="**************"
+            />
+          </label>
+
+          <button
+            type="submit"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </main>
   )
 }
