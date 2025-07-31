6aae692a32aa87291a7812ea527f2d491d00f2d6
Victor Villazón <victorzn23@gmail.com>
2025-07-30 22:02:20 -0500

    semana07, dia01, react redux counter shopping cart app

--- semana07/dia01-react-redux-counter-shooping-cart-app/src/components/ShoppingCart.jsx
+++ semana07/dia01-react-redux-counter-shooping-cart-app/src/components/ShoppingCart.jsx
@@ -1,7 +1,11 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { clearCart } from '../store/cart'
 
 export default function ShoppingCart() {
   const cart = useSelector(state => state.cart)
+
+  const dispatch = useDispatch()
 
   return (
     <section className='w-56'>
@@ -10,21 +14,27 @@ export default function ShoppingCart() {
       <div className='mb-2'>
         <button
           className='bg-violet-400 p-2 rounded-lg cursor-pointer text-white font-medium w-full'
+          onClick={() => dispatch(clearCart())}
         >
           Clean cart
         </button>
       </div>
 
       <ul className='flex flex-col gap-2 mt-4'>
-        <li className='flex flex-col gap-2 font-bold bg-slate-100 p-2 shadow'>
-          <span>title</span>
-          <span>S/ 0.00 (qty: 0)</span>
-          <button
-            className='bg-red-400 p-1 rounded-lg cursor-pointer'
+        {cart && cart.map(product => (
+          <li
+            key={product.id}
+            className='flex flex-col gap-2 font-bold bg-slate-100 p-2 shadow'
           >
-            ❌
-          </button>
-        </li>
+            <span>{product.title}</span>
+            <span>S/ {product.price} (qty: {product.qty})</span>
+            <button
+              className='bg-red-400 p-1 rounded-lg cursor-pointer'
+            >
+              ❌
+            </button>
+          </li>
+        ))}
       </ul>
 
       <div className='mt-4 pt-4 font-bold bg-amber-100 px-4 py-2 flex justify-between'>
--- semana07/dia01-react-redux-counter-shooping-cart-app/src/store/cart.js
+++ semana07/dia01-react-redux-counter-shooping-cart-app/src/store/cart.js
@@ -12,17 +12,40 @@ export const cartSlice = createSlice({
       console.log(state, action)
       const newProduct = action.payload
 
+      const productInCartIndex = state.findIndex(
+        item => item.id === newProduct.id
+      )
+
+      // Si el producto ya existe en el carrito de compras
+      if (productInCartIndex >= 0) { // si fuera -1 el producto no existe
+        return state.map(product => {
+          if (product.id === newProduct.id) {
+            return {
+              ...product,
+              qty: product.qty + 1
+            }
+          }
+
+          return product
+        })
+      }
+
       // Si el producto es nuevo añadimos al carrito
       return [
         ...state,
-        newProduct
+        {
+          ...newProduct,
+          qty: 1
+        }
       ]
+    },
+    // TODO: removeToCart - Remover el producto del carrito de compras
+    clearCart: () => {
+      return [] // Estamos devolviendo el nuevo estado del nuestro slice
     }
-    // removeToCart
-    // clearCart
   }
 })
 
 export default cartSlice.reducer // Expamos el estado inicial y los reducers del slice
 
-export const { addToCart } = cartSlice.actions
+export const { addToCart, clearCart } = cartSlice.actions
