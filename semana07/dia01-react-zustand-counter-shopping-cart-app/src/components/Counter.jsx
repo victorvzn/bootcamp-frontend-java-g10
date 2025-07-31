ece9b11bed51ec2e38dcf48ee08f1a8e8dd0e049
Victor Villazón <victorzn23@gmail.com>
2025-07-30 22:29:48 -0500

    semana07, dia01, react zustand counter shopping cart app

---
+++ semana07/dia01-react-zustand-counter-shopping-cart-app/src/components/Counter.jsx
@@ -0,0 +1,25 @@
+export default function Counter() {
+  return (
+    <div className="border rounded-lg p-2 flex flex-col items-center gap-2">
+      <h3 className="font-bold">Counter with Redux</h3>
+
+      <div className="flex items-center gap-2">
+        <button
+          className="bg-green-400 p-2 min-w-14 rounded-lg font-bold cursor-pointer"
+        >+3</button>
+        <button
+          className="bg-blue-400 p-2 min-w-14 rounded-lg font-bold cursor-pointer"
+        >+1</button>
+        <h1 className="bg-slate-200 p-2 min-w-14 rounded-lg font-bold text-center">
+          0
+        </h1>
+        <button
+          className="bg-blue-400 p-2 min-w-14 rounded-lg font-bold cursor-pointer"
+        >-1</button>
+        <button
+          className="bg-green-400 p-2 min-w-14 rounded-lg font-bold cursor-pointer"
+        >-3</button>
+      </div>
+    </div>
+  )
+}
